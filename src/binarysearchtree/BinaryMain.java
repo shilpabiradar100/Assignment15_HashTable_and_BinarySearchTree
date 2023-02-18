be787a4424f624aab899e0bcdf1dcba84594a6fb
shilpa123 <shilpabiradar100@gmail.com>
2023-02-18 17:07:55 +0530

    ADDED: Created a binary search tree

--- src/binarysearchtree/BinaryMain.java
+++ src/binarysearchtree/BinaryMain.java
@@ -4,47 +4,10 @@ public class BinaryMain {
 
     public static void main(String args[])
     {
-        /**UC1
-         * Create binary search tree
-         */
         BinarySearchTree<Integer> searchTree = new BinarySearchTree<>();
         searchTree.insert(56);
         searchTree.insert(30);
         searchTree.insert(70);
-
-        /**UC2
-         * Create binary search tree with given nodes
-         */
-        BinarySearchTree<Integer> searchTree1 = new BinarySearchTree<>();
-        searchTree1.insert(56);
-        searchTree1.insert(30);
-        searchTree1.insert(70);
-        searchTree1.insert(22);
-        searchTree1.insert(40);
-        searchTree1.insert(60);
-        searchTree1.insert(95);
-        searchTree1.insert(11);
-        searchTree1.insert(65);
-        searchTree1.insert(3);
-        searchTree1.insert(16);
-        searchTree1.insert(63);
-        searchTree1.insert(67);
-
-        //print size of the binary search tree
-        int size = searchTree1.numOfNodes();
-        System.out.println("Size of the binary search tree is " + size);
-
-        /**UC3
-         * Search for a node 63 in binary search tree1
-         */
-        if (searchTree1.searchNode(63))
-        {
-            System.out.println("Node found");
-        }
-        else
-        {
-            System.out.println("Node not found");
-        }
 
     }
 }
--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -2,6 +2,7 @@ package binarysearchtree;
 
 public class BinarySearchTree<T extends Comparable<T>> {
 
+
     BinaryNode<T> root;
 
     /**
@@ -56,55 +57,4 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
-    public int numOfNodes()
-    {
-        return size(root);
-    }
-
-    /**
-     * Method to count number of nodes in binary search tree
-     * @param root
-     * @return
-     */
-    public int size(BinaryNode<T> root)
-    {
-        if(null == root)
-            return 0;
-        int nodesLeftSubtree = size(root.left);
-        int nodesRightSubtree = size(root.right);
-        return nodesLeftSubtree + nodesRightSubtree + 1;
-    }
-
-    /**
-     * Method to search for a node in binary search tree
-     * @param data
-     * @return
-     */
-    public boolean searchNode(T data)
-    {
-        if(root == null)
-        {
-            return false;
-        }
-        else
-        {
-            BinaryNode<T> tempNode = root;
-            while (root != null)
-            {
-                if(data.compareTo(tempNode.data) > 0)
-                {
-                    tempNode = tempNode.right;
-                }
-                else if (data.compareTo(tempNode.data) < 0)
-                {
-                    tempNode = tempNode.left;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
 }
